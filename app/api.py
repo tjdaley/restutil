ec268c99ff36c5efa2af5c1e746ff80740f403c9
Tom Daley <tjd@powerdaley.com>
2020-02-15 11:12:32 -0600

    import util library

--- app/api.py
+++ app/api.py
@@ -6,10 +6,12 @@ Copyright (c) 2020 by Thomas J. Daley, J.D. All Rights Reserved.
 import os
 import flask
 
+import lib.util as UTIL
+
 VERSION = "0.0.1"
 
 app = flask.Flask(__name__)
-app.config['DEBUG'] = True
+app.config['DEBUG'] = UTIL.get_env_bool('FLASK_DEBUG', False)
 
 
 @app.route('/', methods=['GET'])
@@ -17,6 +19,6 @@ def home():
     return {'success': True, 'message': "Hello, World.", 'version': VERSION}
 
 
-port = os.environ['LISTEN_PORT']
-print(f"Listening on {port}.")
+port = UTIL.get_env('LISTEN_PORT', 8081)
+UTIL.logmessage(f"Listening on {port}.")
 app.run(port=port)
