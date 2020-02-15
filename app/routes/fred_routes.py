b76c0a62097cd16e5c8e0dec3fc6785d09578bf0
Tom Daley <tjd@powerdaley.com>
2020-02-15 11:43:11 -0600

    fix some imports

--- app/routes/fred_routes.py
+++ app/routes/fred_routes.py
@@ -3,11 +3,11 @@ fred_routes.py - Flask Routes for handling FRED requests.
 
 Copyright (c) 2020 by Thomas J. Daley, J.D. All Rights Reserved.
 """
-from flask import Blueprint, request, jsonify
 import app.util.util as UTIL
 import app.services.fred as FRED
+from flask import Blueprint, request, jsonify
 
-fred_routes = Blueprint('fred_routes', __name__, template_folder='templates')
+fred_routes = Blueprint('fred_routes', __name__)  #, template_folder='templates')
 
 @fred_routes('/fred/historical_rate/<int:year>/<int:month>/<int:term>/', methods=['GET'])
 def get_historical_rate(year, month, term):
