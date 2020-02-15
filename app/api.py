336e6a94d1d50306b23d9a4228a562d9f5e49b6b
Tom Daley <tjd@powerdaley.com>
2020-02-15 12:53:12 -0600

    generate a sitemap

--- app/api.py
+++ app/api.py
@@ -17,9 +17,25 @@ app.config['DEBUG'] = UTIL.get_env_bool('FLASK_DEBUG', False)
 app.register_blueprint(fred_routes)
 
 
+def list_routes():
+    from urllib.parse import unquote
+    output = []
+    for rule in app.url_map.iter_rules():
+        methods = ','.join(rule.methods)
+
+        line = unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, rule))
+        output.append(line)
+    return output
+
+
 @app.route('/', methods=['GET'])
 def home():
     return {'success': True, 'message': "Hello, World.", 'version': VERSION}
+
+
+@app.route('/sitemap', methods=['GET'])
+def sitemap():
+    return flask.jsonify(list_routes())
 
 
 port = UTIL.get_env('LISTEN_PORT', 8081)
