66fd26e6b358fd87af9c0ce113ceb4771183e09b
Tom Daley <tjd@powerdaley.com>
2020-04-11 10:07:58 -0500

    fix url pattern

--- app/routes/code_search_routes.py
+++ app/routes/code_search_routes.py
@@ -7,10 +7,10 @@ import util.util as UTIL
 import services.codesearch as CODE
 from flask import Blueprint, request, jsonify
 
-code_routes = Blueprint('fred_routes', __name__, template_folder='templates')
+code_routes = Blueprint('code_routes', __name__, template_folder='templates')
 
 
-@code_routes.route('/codesearch/<str:query>/<str:codelist>/', methods=['GET'])
+@code_routes.route('/codesearch/<string:query>/<string:codelist>/', methods=['GET'])
 def get_historical_rate(query, codelist):
     code_searcher = CODE.CodeSearch()
     matches = code_searcher.search(query, codelist)
