3ca15664d804129a8cbacd19cc5430aa5dd4c9d1
Tom Daley <tjd@powerdaley.com>
2020-04-12 06:23:05 -0500

    dynamically build list of codes

--- app/routes/code_search_routes.py
+++ app/routes/code_search_routes.py
@@ -6,12 +6,21 @@ Copyright (c) 2020 by Thomas J. Daley, J.D. All Rights Reserved.
 import util.util as UTIL
 import services.codesearch as CODE
 from flask import Blueprint, request, jsonify
+import urllib.parse
 
 code_routes = Blueprint('code_routes', __name__, template_folder='templates')
 
 
-@code_routes.route('/codesearch/<string:query>/<string:codelist>/', methods=['GET'])
-def get_historical_rate(query, codelist):
+@code_routes.route('/codesearch/search/<string:query>/<string:codelist>/', methods=['GET'])
+def search_codified_laws(query, codelist):
+    u_query = urllib.parse.unquote_plus(query)
+    u_codelist = urllib.parse.unquote_plus(codelist)
     code_searcher = CODE.CodeSearch()
-    matches = code_searcher.search(query, codelist)
+    matches = code_searcher.search(u_query, u_codelist)
     return jsonify(matches)
+
+
+@code_routes.route('/codesearch/list/', methods=['GET'])
+def get_code_list():
+    code_searcher = CODE.CodeSearch()
+    return jsonify(code_searcher.list_codes())
