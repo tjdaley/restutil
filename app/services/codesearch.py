bed82e07a83ad52fabcc1480036298bb8fbb7f14
Tom Daley <tjd@powerdaley.com>
2020-04-12 06:22:48 -0500

    add /list path

--- app/services/codesearch.py
+++ app/services/codesearch.py
@@ -3,19 +3,66 @@ codesearch.py - Search codified laws
 
 Copyright (c) by Thomas J. Daley, J.D.
 """
+import glob
+import os
 import util.functions as FN
 from whoosh.index import open_dir
-from whoosh.qparser import MultifieldParser, FuzzyTermPlugin
+from whoosh.qparser import MultifieldParser, FuzzyTermPlugin, QueryParser
 from collections import namedtuple
+import dotenv
+import json
 
-
-SearchResult = namedtuple(
-    'SearchResult',
-    'code, code_name, title, subtitle, chapter, subchapter, section_number, section_name, text, future_effective_date, highlights'
-)
+# Load environment variables
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv.load_dotenv(dotenv_path)
 
 
 class CodeSearch(object):
+    @staticmethod
+    def list_codes():
+        """
+        Retrieve a list of searchable codes.
+        """
+        code_path = os.environ.get('CODE_PATH')
+        files = glob.glob(f'{code_path}/??.json')
+        codes = []
+        for code_file in files:
+            with open(code_file, 'r') as fp:
+                config = json.load(fp)
+            code_name = config['code_name']
+            if CodeSearch.is_searchable(code_name):
+                searchable = 'Y'
+            else:
+                searchable = 'N'
+            codes.append(
+                {
+                    'code': code_name,
+                    'code_name': config['code_full_name'],
+                    'version': '1.0.0',
+                    'searchable': searchable
+                }
+            )
+        return codes
+
+    @staticmethod
+    def is_searchable(code_name: str) -> bool:
+        """
+        Determine whether there is at least one article having
+        the given code_name.
+
+        Args:
+            code_name (str): Two-letter code abbreviation.
+        Returns:
+            (bool): True if there is at least one article, otherwise False
+        """
+        parser = QueryParser('code', schema=FN.schema())
+        query = parser.parse(code_name.lower())
+        index = open_dir(FN.INDEX_PATH, FN.index_name(None))
+        with index.searcher() as searcher:
+            result = searcher.search(query, limit=1)
+        return result.scored_length() > 0
+
+
     @staticmethod
     def search(query_text, code_list):
         """
@@ -23,6 +70,7 @@ class CodeSearch(object):
 
         Args:
             query (str): Query string entered by user.
+            code_list (str): Space-delimited list of codes to search or '*' or '' for ALL.
         Returns:
             (list): List of SearchResult tuples
         """
@@ -37,29 +85,32 @@ class CodeSearch(object):
             code_clauses = [f'code:{c} ' for c in codes]
             code_clause = ' OR '.join(code_clauses)
             query_text = query_text + ' ' + code_clause
-
+        print(code_clause)
+        print(query_text)
         # Convert query string to query-language query
         query = parser.parse(query_text)
+        print(query)
 
         # Search for results
         documents = []
         with index.searcher() as searcher:
-            result = searcher.search(query)
+            result = searcher.search(query, terms=True)
             result.fragmenter.charlimit = None
             result.fragmenter.maxchars = 1000
             result.fragmenter.surround = 50
             for doc in result:
-                documents.append(SearchResult(
-                    doc.get('code', "NO CODE"),
-                    doc.get('code_name', "NO CODE NAME"),
-                    doc.get('title', "NO TITLE"),
-                    doc.get('subtitle', "NO SUBTITLE"),
-                    doc.get('chapter', "NO CHAPTER"),
-                    doc.get('subchapter', "NO SUBCHAPTER"),
-                    doc.get('section_number', "NO SECTION NUMBER"),
-                    doc.get('section_name', "NO SECTION NAME"),
-                    doc.get('text', "NO TEXT"),
-                    doc.get('future_effective_date', "N/A"),
-                    doc.highlights('text')
-                ))
+                documents.append({
+                    'code': doc.get('code', "NO CODE"),
+                    'code_name': doc.get('code_name', "NO CODE NAME"),
+                    'title': doc.get('title', "NO TITLE"),
+                    'subtitle': doc.get('subtitle', "NO SUBTITLE"),
+                    'chapter': doc.get('chapter', "NO CHAPTER"),
+                    'subchapter': doc.get('subchapter', "NO SUBCHAPTER"),
+                    'section_number': doc.get('section_number', "NO SECTION NUMBER"),
+                    'section_name': doc.get('section_name', "NO SECTION NAME"),
+                    'text': doc.get('text', "NO TEXT"),
+                    'source_text': doc.get('source_text', "SOURCE TEXT NOT AVAILABLE"),
+                    'future_effective_date': doc.get('future_effective_date', "N/A"),
+                    'highlights': doc.highlights('text')
+                })
         return documents
