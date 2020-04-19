aea72877e6287d1a492cbcf6589c097e47b5bb40
Tom Daley <tjd@powerdaley.com>
2020-04-19 10:20:17 -0500

    handle %20 spaces

--- app/routes/code_search_routes.py
+++ app/routes/code_search_routes.py
@@ -14,7 +14,9 @@ code_routes = Blueprint('code_routes', __name__, template_folder='templates')
 @code_routes.route('/codesearch/search/<string:query>/<string:codelist>/', methods=['GET'])
 def search_codified_laws(query, codelist):
     u_query = urllib.parse.unquote_plus(query)
+    u_query = urllib.parse.unquote(u_query)
     u_codelist = urllib.parse.unquote_plus(codelist)
+    u_codelist = urllib.parse.unquote(u_codelist)
     matches = CODE.search(u_query, u_codelist)
     return jsonify(matches)
 
